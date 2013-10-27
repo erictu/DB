56f41937dc435fe35c00336ea48d747b198514e9
terencecho <terencecho12@gmail.com>
2013-10-26 17:59:01 -0700

    added dashabord.c

---
+++ dashboard.c
@@ -0,0 +1,22 @@
+/*
+===============================================================================
+ Name        : dashboard.c
+ Author      : 
+ Version     : 
+ Copyright   : Copyright (C)
+ Description : main definition
+===============================================================================
+*/
+
+
+#include <LPC11xx.h>
+#include <can++.h>
+#include <uart++.h>
+#include "util.h"
+#include <mbed.h>
+#include "pins.h"
+#include "peripherals.h"
+#include "ltc680x.h"
+#include "pitches.h"
+#include "bms_defs.h"
+#include "can_id.h"
