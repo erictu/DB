fc3b6914aee7de05cd05474bfc48a52576ce12fd
Eric Tu <erictctu@gmail.com>
2013-10-27 14:41:40 -0700

    all starter files made

---
+++ pin_def.c
@@ -0,0 +1,9 @@
+ /*
+===============================================================================
+ Name        : pin_def.c
+ Author      : Eric Tu & Terence Cho
+ Version     : 0.1v
+ Copyright   : Copyright (C)
+ Description : Pin definitions for the Dashboard IO module
+===============================================================================
+*/
