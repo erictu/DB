0d0dd9d85c32671387546272f5dfda4b4f8e4184
terencecho <terencecho12@gmail.com>
2013-11-09 22:55:34 -0800

    aded mbedBlink.c, needs to be tested on lpx1114$

---
+++ mbedBlink.c
@@ -0,0 +1,24 @@
+/* 
+* Testing trying to get the lpx1114 to blink it's LED using the mbed library
+* might need to change pin names? this was for a different chip/board
+*/
+
+#include "mbed.h"
+
+Serial pc (USBTX,USBRX);
+
+DigitalOut ledA(dp14);        //p26);
+DigitalOut ledK(dp28);			  //p24);
+
+int main() {
+
+    ledK = 0;
+
+    while(1) {
+        ledA = 1;
+        wait(0.2);
+        ledA = 0;
+        wait(0.2);
+    }
+}
+ 
